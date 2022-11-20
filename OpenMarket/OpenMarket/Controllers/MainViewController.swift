b78e13d58a3dc2472aa70196679755fa1b6c5bcd
yuvinrho <yvrho@naver.com>
2022-11-21 03:51:44 +0900

    feat: 네비게이션 설정

--- OpenMarket/OpenMarket/Controllers/MainViewController.swift
+++ OpenMarket/OpenMarket/Controllers/MainViewController.swift
@@ -6,10 +6,48 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
+    private let addItemBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "plus")
+        return button
+    }()
+
+    private let segmentedControl: UISegmentedControl = {
+        let items = ["LIST", "GRID"]
+        let control = UISegmentedControl(items: items)
+        control.selectedSegmentTintColor = .systemBlue
+        control.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        control.selectedSegmentIndex = 0
+        return control
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureView()
+        configureNavigation()
+    }
+    
+    // MARK: - View 설정
+    private func configureView() {
+        addItemBarButton.target = self
+        addItemBarButton.action = #selector(addItem)
+
+        segmentedControl.addTarget(self, action: #selector(changeItemView), for: .valueChanged)
+    }
+
+    // MARK: - 네비게이션 설정
+    private func configureNavigation() {
+        navigationItem.rightBarButtonItem = addItemBarButton
+        navigationItem.titleView = segmentedControl
+    }
+
+    @objc private func addItem() {
+        print("button pressed.")
+    }
+
+    @objc private func changeItemView() {
+        print("segmented control value changed.")
     }
 }
-
